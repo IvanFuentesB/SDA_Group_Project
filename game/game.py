daa788d28b7cd485de90da52c35403d72e5dfb83
TheLad603 <aposturazvan05@gmail.com>
2025-10-21 23:56:43 +0200

    added player animations

--- game/game.py
+++ game/game.py
@@ -1,8 +1,8 @@
 import pygame
 from dataclasses import dataclass
 
-from scripts.utils import load_image, load_images
-from scripts.entities import PhysicsEntity
+from scripts.utils import load_image, load_images, Animation
+from scripts.entities import PhysicsEntity, Player
 from scripts.tilemap import Tilemap
 from scripts.clouds import Clouds
 
@@ -34,16 +34,18 @@ class Game:
             'stone':load_images('tiles/stone'),
             'player': load_image('entities/player.png'),
             'background': load_image('background.png'),
-            'clouds': load_images('clouds')
+            'clouds': load_images('clouds'),
+            'player/idle': Animation(load_images('entities/player/idle'), img_duration = 6),
+            'player/run': Animation(load_images('entities/player/run'), img_duration=4),
+            'player/jump': Animation(load_images('entities/player/jump')),
+            'player/slide': Animation(load_images('entities/player/slide')),
+            'player/wall_slide': Animation(load_images('entities/player/wall_slide'))
+
         }
         
         self.clouds = Clouds(self.assets['clouds'], count = 16)
         
-        self.player = PhysicsEntity(self, 'player', 
-                                    (50,50), 
-                                    (self.assets['player'].get_width(),
-                                     self.assets['player'].get_height())
-        )
+        self.player = Player(self, (50,50), (self.assets['player'].get_width(), self.assets['player'].get_height()))
         
         self.tilemap = Tilemap(self, tile_size = 16)
         
--- game/scripts/utils.py
+++ game/scripts/utils.py
@@ -5,7 +5,7 @@ import pygame
 BASE_IMG_PATH = 'data/images/'
 
 def load_image(path):
-      img = pygame.image.load(BASE_IMG_PATH + path).convert_alpha()
+      img = pygame.image.load(BASE_IMG_PATH + path).convert()
       img.set_colorkey((0, 0, 0))
       return img
 
@@ -22,5 +22,19 @@ class Animation:
             self.img_duration = img_duration
             self.done = False
             self.frame = 0
+      
+      def copy(self):
+            return Animation(self.images, self.img_duration, self.loop)
+      
+      def update(self):
+            if self.loop:
+                  self.frame = (self.frame + 1) % (self.img_duration * len(self.images))
+            else:
+                  self.frame = min(self.frame + 1, self.img_duration * len(self.images) - 1)
+                  if self.frame >= self.img_duration * len(self.images) - 1:
+                        self.done = True
+      
+      def img(self):
+            return self.images[int(self.frame / self.img_duration)]
       
       
